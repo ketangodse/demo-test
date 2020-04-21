d894376cbc55a30c4bfcc16212f0e762adb2693e
Ketan Godse <ketan.godse@cytel.com>
2020-04-21 15:18:10 +0530

    Added Code

---
+++ LoggingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LoggingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LoggingApp.Log.Logging logging = new Log.Logging("EngineWarpper.Program");
+            Engine.ComputeOutput();            
+            
+            Console.WriteLine("Hello World!");
+            //logging.logger.Debug("Hello World!");
+        }
+    }
+}
